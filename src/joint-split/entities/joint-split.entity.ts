8bf187a674544cb3a7d880dbfe3bdeef28bf4fe4
Azam Dildar <azamdev00@gmail.com>
2024-07-07 12:36:32 +0500

    update joint split entity

--- src/joint-split/entities/joint-split.entity.ts
+++ src/joint-split/entities/joint-split.entity.ts
@@ -1,1 +1,10 @@
-export class JointSplit {}
+import { CustomBaseEntity } from '@/shared/entities/customeBase.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity } from 'typeorm';
+
+@Entity()
+export class JointSplit extends CustomBaseEntity {
+  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  @ApiProperty()
+  SelfAmount: Number;
+}
