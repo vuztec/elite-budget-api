e10a18422d1067d9cf2ea5a6a7ce5744be8ce5b0
Azam Dildar <azamdev00@gmail.com>
2025-06-01 04:34:44 +0500

    update resource table

--- src/resources/entities/resource.entity.ts
+++ src/resources/entities/resource.entity.ts
@@ -1,11 +1,39 @@
+import { Rootuser } from '@/rootusers/entities/rootuser.entity';
 import { CustomBaseEntity } from '@/shared/entities/customeBase.entity';
-import { Column, Entity } from 'typeorm';
+import { ApiHideProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
-export class Resource extends CustomBaseEntity {
+export class Resource {
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Column({ default: null, nullable: true })
   Path: string;
 
   @Column({ default: null, nullable: true })
   Name: string;
+
+  @CreateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  @ApiHideProperty()
+  CreatedAt: Date;
+
+  @UpdateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  @Exclude()
+  @ApiHideProperty()
+  UpdatedAt: Date;
+
+  @ManyToOne(() => Rootuser, (user) => user)
+  CreatedBy: Rootuser;
+
+  @ManyToOne(() => Rootuser, (user) => user)
+  UpdatedBy: Rootuser;
 }
